4b10fced677c54de37bd2e1bebabf21f6a7600d3
nullmames <85548668+nullmames@users.noreply.github.com>
2022-10-26 18:21:02 +1100

    Update validate-gentx.sh
    change build dir to constant

--- scripts/validate-gentx.sh
+++ scripts/validate-gentx.sh
@@ -11,6 +11,7 @@ MAXBOND=90000000
 GENTX_FILE=$(find ./$CHAIN_ID/gentx -iname "*.json")
 LEN_GENTX=$(echo ${#GENTX_FILE})
 DEFUNDD_TAG="v0.1.0-alpha"
+BUILD_DIR="~/go/bin/defundd"
 
 # Gentx Start date
 start="2022-10-24 01:00:00Z"
@@ -57,11 +58,11 @@ else
     cd defund
     git checkout $DEFUNDD_TAG
     make install
-    chmod +x ./bin/defundd
+    chmod +x $BUILD_DIR
 
-    ./bin/defundd keys add $RANDOM_KEY --keyring-backend test --home $DEFUNDD_HOME
+    $BUILD_DIR keys add $RANDOM_KEY --keyring-backend test --home $DEFUNDD_HOME
 
-    ./bin/defundd init --chain-id $CHAIN_ID validator --home $DEFUNDD_HOME
+    $BUILD_DIR init --chain-id $CHAIN_ID validator --home $DEFUNDD_HOME
 
     echo "..........Fetching genesis......."
     rm -rf $DEFUNDD_HOME/config/genesis.json
@@ -92,7 +93,7 @@ else
                 exit 1
             fi
 
-            ./bin/defundd add-genesis-account $(jq -r '.body.messages[0].delegator_address' $line) $VALIDATOR_COINS --home $DEFUNDD_HOME
+            $BUILD_DIR add-genesis-account $(jq -r '.body.messages[0].delegator_address' $line) $VALIDATOR_COINS --home $DEFUNDD_HOME
         done
 
     mkdir -p $DEFUNDD_HOME/config/gentx/
@@ -101,20 +102,20 @@ else
     cp -r ../$CHAIN_ID/gentx/* $DEFUNDD_HOME/config/gentx/
 
     echo "..........Collecting gentxs......."
-    ./bin/defundd collect-gentxs --home $DEFUNDD_HOME &> log.txt
+    $BUILD_DIR collect-gentxs --home $DEFUNDD_HOME &> log.txt
     sed -i '/persistent_peers =/c\persistent_peers = ""' $DEFUNDD_HOME/config/config.toml
     sed -i '/minimum-gas-prices =/c\minimum-gas-prices = "0.25ufetf"' $DEFUNDD_HOME/config/app.toml
 
-    ./bin/defundd validate-genesis --home $DEFUNDD_HOME
+    $BUILD_DIR validate-genesis --home $DEFUNDD_HOME
 
     echo "..........Starting node......."
-    ./bin/defundd start --home $DEFUNDD_HOME &
+    $BUILD_DIR start --home $DEFUNDD_HOME &
 
     sleep 90s
 
     echo "...checking network status.."
 
-    ./bin/defundd status --node http://localhost:26657
+    $BUILD_DIR status --node http://localhost:26657
 
     echo "...Cleaning the stuff..."
     killall defundd >/dev/null 2>&1
